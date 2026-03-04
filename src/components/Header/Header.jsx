eaff83e6566f5458328541660e7916990e9a8bc5
Honorine <htuyishime208@gmail.com>
2026-03-04 15:57:12 -0700

    it is submitting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,9 @@ const dateStr= now.toLocaleDateString("default",{
   return (
         <header className="header"> 
         <div className="header__side">
+          <Link to ="/profile">
         < img src={logo} alt="WTWR logo" className="header__logo"/>
+        </Link>
         <p className="header__place" >
           <time className="header__datime" dateTime= {now}> {dateStr}</time> , {weatherData.city}
           </p>
