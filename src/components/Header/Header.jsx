fb94f9e3cd10fc5dbde971043e505cf2f8a32538
Honorine <htuyishime208@gmail.com>
2026-03-06 15:17:13 -0700

    ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,8 +13,12 @@ const dateStr= now.toLocaleDateString("default",{
   return (
         <header className="header"> 
         <div className="header__side">
-          <Link to="/logo">
-        < img src={logo} alt="WTWR logo" className="header__logo"/> </Link>
+           <nav>
+         <Link to="/">Home</Link>
+    </nav>
+  
+       
+        < img src={logo} alt="WTWR logo" className="header__logo"/> 
         <p className="header__place" >
           <time className="header__datime" dateTime= {now}> {dateStr}</time> , {weatherData.city}
           </p>
